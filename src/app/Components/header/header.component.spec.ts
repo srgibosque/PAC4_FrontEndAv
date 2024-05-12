ca88b9eed90aded4c76951c61524d4505eb92895
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-05-12 14:09:47 +0200

    testing for the routes in headerComponent

--- src/app/Components/header/header.component.spec.ts
+++ src/app/Components/header/header.component.spec.ts
@@ -9,14 +9,21 @@ class TemporalComponentForRoutes {}
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule.withRoutes([
           { path: 'home', component: TemporalComponentForRoutes},
-        ])
-      ],
+          { path: 'dashboard', component: TemporalComponentForRoutes },
+          { path: 'login', component: TemporalComponentForRoutes },
+          { path: 'register', component: TemporalComponentForRoutes },
+          { path: 'profile', component: TemporalComponentForRoutes },
+          { path: 'posts', component: TemporalComponentForRoutes },
+          { path: 'categories', component: TemporalComponentForRoutes }
+          ])
+        ],
       declarations: [HeaderComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
@@ -25,6 +32,7 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
@@ -32,14 +40,48 @@ describe('HeaderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should navigate to home', () => {
-  //   const router = TestBed.inject(Router);
-  //   // check that the router is listening to the 'navigateByUrl' method
-  //   const spy = spyOn(router, 'navigateByUrl')
-  //   //execute the home() method
-  //   component.home();
-  //   //s'espera que es llanci un 'navigateByUrl' amb argument home
-  //   expect(spy).toHaveBeenCalledWith('home');
-  // });
+  it('should navigate to home', () => {
+    // check that the router is listening to the 'navigateByUrl' method
+    const spy = spyOn(router, 'navigateByUrl');
+    //execute the navigationTo() method
+    component.navigationTo('home');
+    //s'espera que es llanci un 'navigateByUrl' amb argument home
+    expect(spy).toHaveBeenCalledWith('home');
+  });
 
+  it('should navigate to dashboard', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    component.navigationTo('dashboard');
+    expect(spy).toHaveBeenCalledWith('dashboard');
+  });
+
+  it('should navigate to login', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    component.navigationTo('login');
+    expect(spy).toHaveBeenCalledWith('login');
+  });
+
+  it('should navigate to register', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    component.navigationTo('register');
+    expect(spy).toHaveBeenCalledWith('register');
+  });
+
+  it('should navigate to profile', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    component.navigationTo('profile');
+    expect(spy).toHaveBeenCalledWith('profile');
+  });
+
+  it('should navigate to adminPosts', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    component.navigationTo('posts');
+    expect(spy).toHaveBeenCalledWith('posts');
+  });
+
+  it('should navigate to adminCategories', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    component.navigationTo('categories');
+    expect(spy).toHaveBeenCalledWith('categories');
+  });
 });
